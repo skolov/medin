2b5bba2368f6bbbaacc5fcd2915a933d32c751c0
Александр Родин <alexandr.rodin@sitesoft.ru>
2018-08-04 20:52:51 +0500

    Правка структуры в js

--- src/js/blocks.js
+++
@@ -1,6 +0,0 @@
-// делает inlude для всех js файлов в папке blocks
-var context = require.context("../blocks", true, /\.js$/);
-var obj = {};
-context.keys().forEach(function (key) {
-    obj[key] = context(key);
-});
--- src/js/other.js
+++ src/js/other.js
@@ -1,4 +1,5 @@
 const requireAll = r => r.keys().forEach(r);
-
 // include svg-sprite
 requireAll(require.context('../svg-sprite/', true, /\.svg$/));
+// include blocks
+requireAll(require.context('../blocks/', true, /\.js$/));
