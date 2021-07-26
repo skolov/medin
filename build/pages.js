94b4c78efd5d73407a1cf6cb540532bc78ead1d0
Elisaveta Katyreva <elisaveta.katyreva@sitesoft.ru>
2021-07-26 15:30:24 +0500

    feat(uslugi-i-tseny): страница добавлена со стенда as is

--- build/pages.js
+++ build/pages.js
@@ -9,44 +9,55 @@ module.exports = [
     title: 'index',
     path: 'pages/index',
     entry: {
-      markup_pageList: path.join(paths.app, 'pug/pages/index')
+      markup_pageList: path.join(paths.app, 'pug/pages/index'),
     },
     template: path.join(paths.app, 'pug/pages/index/index.pug'),
 
     // Подключаемые зависимости на страницу
-    chunks: ['runtime', 'vendors', 'app', 'markup_pageList']
+    chunks: ['runtime', 'vendors', 'app', 'markup_pageList'],
   }),
   parts.page({
     title: 'nojs-browser',
     path: 'pages/nojs-browser',
     entry: {
-      'nojs-browser': path.join(paths.app, 'pug/pages/nojs-browser')
+      'nojs-browser': path.join(paths.app, 'pug/pages/nojs-browser'),
     },
     template: path.join(paths.app, 'pug/pages/nojs-browser/index.pug'),
 
     // Подключаемые зависимости на страницу
-    chunks: ['runtime', 'nojs-browser']
+    chunks: ['runtime', 'nojs-browser'],
   }),
   parts.page({
     title: 'update-browser',
     path: 'pages/update-browser',
     entry: {
-      'update-browser': path.join(paths.app, 'pug/pages/update-browser')
+      'update-browser': path.join(paths.app, 'pug/pages/update-browser'),
     },
     template: path.join(paths.app, 'pug/pages/update-browser/index.pug'),
 
     // Подключаемые зависимости на страницу
-    chunks: ['runtime', 'update-browser']
+    chunks: ['runtime', 'update-browser'],
   }),
   parts.page({
     title: 'home',
     path: 'pages/home',
     entry: {
-      markup_home: path.join(paths.app, 'pug/pages/home')
+      markup_home: path.join(paths.app, 'pug/pages/home'),
     },
     template: path.join(paths.app, 'pug/pages/home/index.pug'),
 
     // Подключаемые зависимости на страницу
-    chunks: ['runtime', 'vendors', 'app', 'markup_home']
+    chunks: ['runtime', 'vendors', 'app', 'markup_home'],
+  }),
+  parts.page({
+    title: 'uslugi-i-tseny',
+    path: 'pages/uslugi-i-tseny',
+    entry: {
+      markup_uslugi_i_tseny: path.join(paths.app, 'pug/pages/uslugi-i-tseny'),
+    },
+    template: path.join(paths.app, 'pug/pages/uslugi-i-tseny/index.pug'),
+
+    // Подключаемые зависимости на страницу
+    chunks: ['runtime', 'vendors', 'app', 'markup_uslugi_i_tseny'],
   }),
 ]
